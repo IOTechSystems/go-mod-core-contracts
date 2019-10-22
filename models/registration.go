6a042987f442a3ccb788451d7da2f14e8d36ea0f
Felix Ting <felix@iotechsys.com>
2019-10-22 14:13:43 +0800

    ITM-184 Export Service enhancement for Accenture [Iteration 3]
    Add MaxRetentionEvents and MaxBatchEvents into Registration struct
    
    Signed-off-by: Felix Ting <felix@iotechsys.com>

--- models/registration.go
+++ models/registration.go
@@ -62,44 +62,49 @@ const (
 const (
 	ProcessFrequencyZero            = "0"
 	ProcessFrequencyMinimumDuration = 1 //second
+	DefaultMaxRetentionEvents       = 20000
 )
 
 // Registration - Defines the registration details
 // on the part of north side export clients
 type Registration struct {
-	ID               string            `json:"id,omitempty"`
-	Created          int64             `json:"created"`
-	Modified         int64             `json:"modified"`
-	Origin           int64             `json:"origin"`
-	Name             string            `json:"name,omitempty"`
-	Addressable      Addressable       `json:"addressable,omitempty"`
-	Format           string            `json:"format,omitempty"`
-	Filter           Filter            `json:"filter,omitempty"`
-	Encryption       EncryptionDetails `json:"encryption,omitempty"`
-	Compression      string            `json:"compression,omitempty"`
-	Enable           bool              `json:"enable"`
-	Destination      string            `json:"destination,omitempty"`
-	ProcessFrequency string            `json:"processFrequency,omitempty"`
-	isValidated      bool              // internal member used for validation check
+	ID                 string            `json:"id,omitempty"`
+	Created            int64             `json:"created"`
+	Modified           int64             `json:"modified"`
+	Origin             int64             `json:"origin"`
+	Name               string            `json:"name,omitempty"`
+	Addressable        Addressable       `json:"addressable,omitempty"`
+	Format             string            `json:"format,omitempty"`
+	Filter             Filter            `json:"filter,omitempty"`
+	Encryption         EncryptionDetails `json:"encryption,omitempty"`
+	Compression        string            `json:"compression,omitempty"`
+	Enable             bool              `json:"enable"`
+	Destination        string            `json:"destination,omitempty"`
+	ProcessFrequency   string            `json:"processFrequency,omitempty"`
+	MaxRetentionEvents uint              `json:"maxRetentionEvents,omitempty"`
+	MaxBatchEvents     uint              `json:"maxBatchEvents,omitempty"`
+	isValidated        bool              // internal member used for validation check
 }
 
 // UnmarshalJSON implements the Unmarshaler interface for the DeviceService type
 func (r *Registration) UnmarshalJSON(data []byte) error {
 	var err error
 	type Alias struct {
-		ID               *string           `json:"id"`
-		Created          int64             `json:"created"`
-		Modified         int64             `json:"modified"`
-		Origin           int64             `json:"origin"`
-		Name             *string           `json:"name"`
-		Addressable      Addressable       `json:"addressable"`
-		Format           *string           `json:"format"`
-		Filter           Filter            `json:"filter"`
-		Encryption       EncryptionDetails `json:"encryption"`
-		Compression      *string           `json:"compression"`
-		Enable           bool              `json:"enable"`
-		Destination      *string           `json:"destination"`
-		ProcessFrequency string            `json:"processFrequency"`
+		ID                 *string           `json:"id"`
+		Created            int64             `json:"created"`
+		Modified           int64             `json:"modified"`
+		Origin             int64             `json:"origin"`
+		Name               *string           `json:"name"`
+		Addressable        Addressable       `json:"addressable"`
+		Format             *string           `json:"format"`
+		Filter             Filter            `json:"filter"`
+		Encryption         EncryptionDetails `json:"encryption"`
+		Compression        *string           `json:"compression"`
+		Enable             bool              `json:"enable"`
+		Destination        *string           `json:"destination"`
+		ProcessFrequency   string            `json:"processFrequency"`
+		MaxRetentionEvents uint              `json:"maxRetentionEvents"`
+		MaxBatchEvents     uint              `json:"maxBatchEvents"`
 	}
 	a := Alias{}
 
@@ -132,6 +137,11 @@ func (r *Registration) UnmarshalJSON(data []byte) error {
 	r.Encryption = a.Encryption
 	r.Enable = a.Enable
 	r.ProcessFrequency = a.ProcessFrequency
+	r.MaxBatchEvents = a.MaxBatchEvents
+	r.MaxRetentionEvents = a.MaxRetentionEvents
+	if r.MaxRetentionEvents == 0 {
+		r.MaxRetentionEvents = DefaultMaxRetentionEvents
+	}
 
 	r.isValidated, err = r.Validate()
 
