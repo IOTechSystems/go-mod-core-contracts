b121c9a24c7a9efe9f0761f512bb9f9d08b56435
Bruce Huang <weichou1229@gmail.com>
2022-08-01 15:52:11 +0800

    EDX-3082 Add XRT properties conversion (#144)
    * EDX-3082 Add XRT properties conversion
    
    Add conversion func to convert the protocol properties between EdgeX model and XRT model
    
    Signed-off-by: bruce <weichou1229@gmail.com>
    
    * EDX-3082 Rename int and float properties list
    
    Signed-off-by: bruce <weichou1229@gmail.com>

---
+++ common/xpertconstants.go
@@ -0,0 +1,34 @@
+// Copyright (C) 2022 IOTech Ltd
+
+package common
+
+const (
+	BacnetIP             = "BACnet-IP"
+	BacnetMSTP           = "BACnet-MSTP"
+	BacnetDeviceInstance = "DeviceInstance"
+
+	Gps                   = "GPS"
+	GpsGpsdPort           = "GpsdPort"
+	GpsGpsdRetries        = "GpsdRetries"
+	GpsGpsdConnTimeout    = "GpsdConnTimeout"
+	GpsGpsdRequestTimeout = "GpsdRequestTimeout"
+
+	ModbusTcp      = "modbus-tcp"
+	ModbusRtu      = "modbus-rtu"
+	ModbusUnitID   = "UnitID"
+	ModbusPort     = "Port"
+	ModbusBaudRate = "BaudRate"
+	ModbusDataBits = "DataBits"
+	ModbusStopBits = "StopBits"
+
+	Opcua                           = "OPC-UA"
+	OpcuaRequestedSessionTimeout    = "RequestedSessionTimeout"
+	OpcuaBrowseDepth                = "BrowseDepth"
+	OpcuaBrowsePublishInterval      = "BrowsePublishInterval"
+	OpcuaConnectionReadingPostDelay = "ConnectionReadingPostDelay"
+	OpcuaIDType                     = "IDType"
+
+	S7     = "S7"
+	S7Rack = "Rack"
+	S7Slot = "Slot"
+)
