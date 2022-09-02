878c82c6d4860ad64a0a826f759ac3176af9e4b8
Lindsey Cheng <beckysocute@gmail.com>
2022-09-02 17:42:27 +0800

    EDX-3207 Add microservices DTOs for sys-mgmt (#149)
    * EDX-3207 Add microservices DTOs for sys-mgmt
    
    Add microservices DTOs from edgex-go-private sys-mgmt-agent.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

---
+++ dtos/responses/microservice_test.go
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2022 IOTech Ltd
+//
+
+package responses
+
+import (
+	"testing"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMultiMicroServicesResponse(t *testing.T) {
+	expectedRequestId := "123456"
+	expectedStatusCode := 200
+	expectedMessage := "unit test message"
+	expectedMicroServices := []dtos.MicroService{
+		{
+			ID:    "7102e1adab50bfc9894e88d0afe903c0f0cf69d5305cbac86b7f3c6ee28afde4",
+			Names: []string{"/core-data"},
+			Image: "iotechsys/dev-edgexpert-core-data:2.2.dev",
+		},
+		{
+			ID:    "bbf4d4be01004080043a29185c8e5d01a3973f082fa069898c2432b0f44996ff",
+			Names: []string{"/core-command"},
+			Image: "iotechsys/dev-edgexpert-core-command:2.2.dev",
+		},
+	}
+	actual := NewMultiMicroServicesResponse(expectedRequestId, expectedMessage, expectedStatusCode, expectedMicroServices)
+
+	assert.Equal(t, expectedRequestId, actual.RequestId)
+	assert.Equal(t, expectedStatusCode, actual.StatusCode)
+	assert.Equal(t, expectedMessage, actual.Message)
+	assert.Equal(t, expectedMicroServices, actual.MicroServices)
+}
