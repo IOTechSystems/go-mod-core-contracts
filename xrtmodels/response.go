b65cf5475ce5d349dd6296a5b1d313a4f3a811a4
Bruce Huang <weichou1229@gmail.com>
2022-12-21 18:03:36 +0800

    PS-455 Add XRT to EdgeX Event conversion func (#160)
    - Add sourceName and tags fields to XRT reading
    - Add XRT to EdgeX Event conversion func
    
    Signed-off-by: bruce <weichou1229@gmail.com>

--- xrtmodels/response.go
+++ xrtmodels/response.go
@@ -56,14 +56,19 @@ type MultiResourcesResponse struct {
 
 type MultiResourcesResult struct {
 	BaseResult `json:",inline"`
-	Device     string             `json:"device"`
-	Profile    string             `json:"profile"`
-	Readings   map[string]Reading `json:"readings"`
+	Device     string                 `json:"device"`
+	Profile    string                 `json:"profile"`
+	SourceName string                 `json:"sourceName"`
+	Readings   map[string]Reading     `json:"readings"`
+	Tags       map[string]interface{} `json:"tags"`
+	Type       string                 `json:"type"`
 }
 
 type Reading struct {
-	Value interface{} `json:"value"`
-	Type  string      `json:"type"`
+	Value  interface{}            `json:"value"`
+	Type   string                 `json:"type"`
+	Origin int64                  `json:"origin"`
+	Tags   map[string]interface{} `json:"tags"`
 }
 
 type MultiDevicesResponse struct {
--- xrtmodels/xrtconv.go
+++ xrtmodels/xrtconv.go
@@ -3,10 +3,12 @@
 package xrtmodels
 
 import (
+	"encoding/base64"
 	"fmt"
 	"strconv"
 
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/common"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/errors"
 )
 
@@ -120,4 +122,322 @@ func propertyConversionList(protocol string) ([]string, []string, []string) {
 			common.EtherNetIPMajorRevision, common.EtherNetIPMinorRevision}
 	}
 	return intProperties, floatProperties, boolProperties
+}
+
+func ToEdgeXV2EventDTO(xrtEvent MultiResourcesResult) (dtos.Event, errors.EdgeX) {
+	event := dtos.Event{
+		DeviceName:  xrtEvent.Device,
+		ProfileName: xrtEvent.Profile,
+		SourceName:  xrtEvent.SourceName,
+		Tags:        xrtEvent.Tags,
+		Readings:    make([]dtos.BaseReading, len(xrtEvent.Readings)),
+	}
+
+	index := 0
+	for resourceName, reading := range xrtEvent.Readings {
+		valueType, err := common.NormalizeValueType(reading.Type)
+		if err != nil {
+			return event, errors.NewCommonEdgeXWrapper(err)
+		}
+		value, err := ParseXRTReadingValue(valueType, reading.Value)
+		if err != nil {
+			return event, errors.NewCommonEdgeXWrapper(err)
+		}
+
+		switch valueType {
+		case common.ValueTypeBinary:
+			if data, ok := value.([]byte); ok {
+				event.Readings[index] = dtos.NewBinaryReading(xrtEvent.Profile, xrtEvent.Device, resourceName, data, "")
+			} else {
+				return event, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid binary value '%v'", value), nil)
+			}
+		case common.ValueTypeObject:
+			event.Readings[index] = dtos.NewObjectReading(xrtEvent.Profile, xrtEvent.Device, resourceName, value)
+
+		default:
+			event.Readings[index], err = dtos.NewSimpleReading(xrtEvent.Profile, xrtEvent.Device, resourceName, valueType, value)
+			if err != nil {
+				return event, errors.NewCommonEdgeXWrapper(err)
+			}
+		}
+		event.Readings[index].Origin = reading.Origin
+		event.Readings[index].Tags = reading.Tags
+		event.Origin = reading.Origin
+		index++
+	}
+
+	return event, nil
+}
+
+// ParseXRTReadingValue parses the XRT reading value to EdgeX reading value
+func ParseXRTReadingValue(valueType string, reading interface{}) (interface{}, errors.EdgeX) {
+	// Since we receive the reading in JSON format, the JSON lib will unmarshal the reading to specified data type:
+	// bool for JSON booleans,  float64 for JSON numbers, nil for JSON null
+	// string for JSON strings, []interface{} for JSON arrays, map[string]interface{} for JSON objects
+	var err error
+	var val interface{}
+	switch valueType {
+	case common.ValueTypeString:
+		strValue, ok := reading.(string)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid string value '%v'", reading), nil)
+		}
+		val = strValue
+	case common.ValueTypeBool:
+		boolValue, ok := reading.(bool)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid bool value '%v'", reading), nil)
+		}
+		val = boolValue
+	case common.ValueTypeUint8:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid numbers '%v'", reading), nil)
+		}
+		val = uint8(float64Value)
+	case common.ValueTypeUint16:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid numbers '%v'", reading), nil)
+		}
+		val = uint16(float64Value)
+	case common.ValueTypeUint32:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = uint32(float64Value)
+	case common.ValueTypeUint64:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = uint64(float64Value)
+	case common.ValueTypeInt8:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = int8(float64Value)
+	case common.ValueTypeInt16:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = int16(float64Value)
+	case common.ValueTypeInt32:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = int32(float64Value)
+	case common.ValueTypeInt64:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = int64(float64Value)
+	case common.ValueTypeFloat32:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = float32(float64Value)
+	case common.ValueTypeFloat64:
+		float64Value, ok := reading.(float64)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", reading), nil)
+		}
+		val = float64Value
+	case common.ValueTypeBinary:
+		// XRT transfer binary data in base64 encoded string
+		strValue, ok := reading.(string)
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid string value '%v'", reading), nil)
+		}
+		val, err = base64.StdEncoding.DecodeString(strValue)
+		if err != nil {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("fail to decode the base64 string '%v'", reading), err)
+		}
+	case common.ValueTypeBoolArray:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		boolArray := make([]bool, len(interfaceArray))
+		for i, v := range interfaceArray {
+			boolValue, ok := v.(bool)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid bool value '%v'", v), nil)
+			}
+			boolArray[i] = boolValue
+		}
+		val = boolArray
+	case common.ValueTypeStringArray:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		stringArray := make([]string, len(interfaceArray))
+		for i, v := range interfaceArray {
+			strValue, ok := v.(string)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid string value '%v'", v), nil)
+			}
+			stringArray[i] = strValue
+		}
+		val = stringArray
+	case common.ValueTypeUint8Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		uint8Array := make([]uint8, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid nunmber '%v'", v), nil)
+			}
+			uint8Array[i] = uint8(float64Value)
+		}
+		val = uint8Array
+	case common.ValueTypeUint16Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		uint16Array := make([]uint16, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid nunmber '%v'", v), nil)
+			}
+			uint16Array[i] = uint16(float64Value)
+		}
+		val = uint16Array
+	case common.ValueTypeUint32Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		uint32Array := make([]uint32, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid nunmber '%v'", v), nil)
+			}
+			uint32Array[i] = uint32(float64Value)
+		}
+		val = uint32Array
+	case common.ValueTypeUint64Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		uint64Array := make([]uint64, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid nunmber '%v'", v), nil)
+			}
+			uint64Array[i] = uint64(float64Value)
+		}
+		val = uint64Array
+	case common.ValueTypeInt8Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		int8Array := make([]int8, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid nunmber '%v'", v), nil)
+			}
+			int8Array[i] = int8(float64Value)
+		}
+		val = int8Array
+	case common.ValueTypeInt16Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		int16Array := make([]int16, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid nunmber '%v'", v), nil)
+			}
+			int16Array[i] = int16(float64Value)
+		}
+		val = int16Array
+	case common.ValueTypeInt32Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		int32Array := make([]int32, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", v), nil)
+			}
+			int32Array[i] = int32(float64Value)
+		}
+		val = int32Array
+	case common.ValueTypeInt64Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		int64Array := make([]int64, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", v), nil)
+			}
+			int64Array[i] = int64(float64Value)
+		}
+		val = int64Array
+	case common.ValueTypeFloat32Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		float32Array := make([]float32, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", v), nil)
+			}
+			float32Array[i] = float32(float64Value)
+		}
+		val = float32Array
+	case common.ValueTypeFloat64Array:
+		interfaceArray, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		float64Array := make([]float64, len(interfaceArray))
+		for i, v := range interfaceArray {
+			float64Value, ok := v.(float64)
+			if !ok {
+				return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid number '%v'", v), nil)
+			}
+			float64Array[i] = float64Value
+		}
+		val = float64Array
+	case common.ValueTypeObject:
+		val = reading
+	case common.ValueTypeObjectArray:
+		_, ok := reading.([]interface{})
+		if !ok {
+			return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("invalid array '%v'", reading), nil)
+		}
+		val = reading
+	default:
+		return nil, errors.NewCommonEdgeX(errors.KindContractInvalid, fmt.Sprintf("none supported value type '%s'", valueType), nil)
+	}
+
+	return val, nil
 }
