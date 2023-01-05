6c2e67a0c4072cc0bd50e76f40760e619092f178
Felix Ting <felix@iotechsys.com>
2023-01-05 22:35:36 +0800

    EDX-3362 Add support for TOML-formatted service configuration
    Signed-off-by: Felix Ting <felix@iotechsys.com>

--- clients/interfaces/general.go
+++ clients/interfaces/general.go
@@ -17,4 +17,7 @@ type GeneralClient interface {
 	FetchConfiguration(ctx context.Context) (common.ConfigResponse, errors.EdgeX)
 	// FetchMetrics obtains metrics information from the target service.
 	FetchMetrics(ctx context.Context) (common.MetricsResponse, errors.EdgeX)
+	// XpertFetchConfiguration obtains configuration information from the target service.
+	// In comparison with FetchConfiguration, this function supports both JSON and TOML formats.
+	XpertFetchConfiguration(ctx context.Context) (common.ConfigResponse, errors.EdgeX)
 }
