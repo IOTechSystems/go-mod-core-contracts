8c08cd11f6ea108808d2f409c9abbb434bb4880f
Lindsey Cheng <beckysocute@gmail.com>
2023-03-07 16:32:26 +0800

    EDX-3605 Remove CreateRedisTlsConfig common function
    Remove CreateRedisTlsConfig common function.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

--- common/tls.go
+++ common/tls.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2022 IOTech Ltd
+// Copyright (C) 2022-2023 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -8,7 +8,6 @@ package common
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"path/filepath"
 
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/errors"
 )
@@ -35,32 +34,7 @@ const (
 	EnvMessageBusMqttTls   = "EDGEXPERT_MESSAGEBUS_MQTT_TLS"
 )
 
-// CreateRedisTlsConfig loads TLS certificates from specified path and creates Redis TLS config
-func CreateRedisTlsConfig() (tlsConfig *tls.Config, err error) {
-	redisKeyFilePath := filepath.Join(RedisTlsCertOutputDir, RedisKeyFileName)
-	redisCertFilePath := filepath.Join(RedisTlsCertOutputDir, RedisCertFileName)
-	cert, err := tls.LoadX509KeyPair(redisCertFilePath, redisKeyFilePath)
-	if err != nil {
-		return tlsConfig, errors.NewCommonEdgeX(errors.KindServerError, "fail to parse the Redis TLS key pair", err)
-	}
-	certificate, err := x509.ParseCertificate(cert.Certificate[0])
-	if err != nil {
-		return tlsConfig, errors.NewCommonEdgeX(errors.KindServerError, "fail to parse the certificate", err)
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AddCert(certificate)
-
-	tlsConfig = &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      caCertPool,
-		MinVersion:   tls.VersionTLS12,
-		// skip server side SSL verification, primarily for self-signed certs
-		InsecureSkipVerify: true, // nolint:gosec
-	}
-	return tlsConfig, nil
-}
-
-// CreateRedisTlsConfig loads TLS certificates from PEM encoded data and creates Redis TLS config
+// CreateRedisTlsConfigFromPEM loads TLS certificates from PEM encoded data and creates Redis TLS config
 func CreateRedisTlsConfigFromPEM(certPEMBlock, keyPEMBlock []byte) (*tls.Config, errors.EdgeX) {
 	var tlsConfig *tls.Config
 	cert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
