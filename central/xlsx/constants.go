ac578c75d43393d9aebaf7596dacd53dfefca8e2
Lindsey Cheng <beckysocute@gmail.com>
2023-10-30 11:45:12 +0800

    EDX-4328 Add parse excel to Device DTO function (#199)
    * EDX-4328 Add parse excel to Device DTO function
    
    Add parse excel to Device DTO function.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Fix linter errors
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Remove unused xlsxreader module
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * EDX-4328 Update package name to centralutils
    
    Update package name to centralutils and add the Xlsx interface abstraction.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Package rename and refactor convert xlsx methods
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Refactor convert methods to be more generic
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Update slices package import path
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Update newDeviceXlsx factory method
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * Remove the extra switch condition in ConvertXlsx func
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    ---------
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

---
+++ central/xlsx/constants.go
@@ -0,0 +1,31 @@
+//
+// Copyright (C) 2023 IOTech Ltd
+//
+
+package xlsx
+
+// constants relates to the stylesheet names
+const (
+	devicesSheetName      = "Devices"
+	mappingTableSheetName = "MappingTable"
+	autoEventsSheetName   = "AutoEvents"
+)
+
+// constants relates to the header names
+const (
+	objectCol       = "Object"
+	pathCol         = "Path"
+	defaultValueCol = "Default Value"
+)
+
+// constants relates to the Device DTO field names
+const (
+	protocols    = "Protocols"
+	protocolName = "ProtocolName"
+	autoEvents   = "AutoEvents"
+)
+
+// constants relates to the Device protocol property keys
+const (
+	modbusRTUKey = "modbus-rtu"
+)
