ad6e8acd6d1b247b37cd74e7bfc188e92f26903c
bruce <weichou1229@gmail.com>
2024-01-16 12:21:59 +0800

    EDX-3971 Resolve conflicts with v3.1 changes
    Resolve conflicts with v3.1 changes
    
    Signed-off-by: bruce <weichou1229@gmail.com>

--- clients/interfaces/registry.go
+++ clients/interfaces/registry.go
@@ -12,7 +12,7 @@ import (
 	"github.com/edgexfoundry/go-mod-core-contracts/v3/errors"
 )
 
-// RegistryClient defines the interface for interactions with the registry endpoint on the Edge Xpert core-keeper service.
+// RegistryClient defines the interface for interactions with the registry endpoint on the Edge Central core-keeper service.
 type RegistryClient interface {
 	Register(context.Context, requests.AddRegistrationRequest) errors.EdgeX
 	UpdateRegister(context.Context, requests.AddRegistrationRequest) errors.EdgeX
---
+++ errors/central.go
@@ -0,0 +1,5 @@
+package errors
+
+const (
+	KindUnsupportedMediaType ErrKind = "UnsupportedMediaType"
+)
