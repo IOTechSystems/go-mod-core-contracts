e38c2f58495f5baa72dbae1a50e8b157014c4e11
Lindsey Cheng <beckysocute@gmail.com>
2024-07-25 12:30:48 +0800

    feat: Add security bootstrapper key for postgres db
    Resolves edgexfoundry/edgex-go#4836.
    Add the security bootstrapper key for postgres db.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

--- common/constants.go
+++ common/constants.go
@@ -329,6 +329,7 @@ const (
 	SecurityProxySetupServiceKey        = "security-proxy-setup"
 	SecurityFileTokenProviderServiceKey = "security-file-token-provider"
 	SecurityBootstrapperKey             = "security-bootstrapper"
+	SecurityBootstrapperPostgresKey     = "security-bootstrapper-postgres"
 	SecurityBootstrapperRedisKey        = "security-bootstrapper-redis"
 	SecuritySpiffeTokenProviderKey      = "security-spiffe-token-provider" // nolint:gosec
 )
