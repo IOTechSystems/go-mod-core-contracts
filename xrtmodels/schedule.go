52e603b3de2cb0436c4de88bcd2b6a2b0f5f49d7
bruce <weichou1229@gmail.com>
2024-10-28 16:36:17 +0800

    feat: Add onChangeThreshold in Device AutoEvent
    The onChange flag in Device AutoEvent can prevent any non changed values sent out. The onChangeThreshold could provide the advanced feature, and the default value is 0, any changed value that exceeds (>) bounds shall be published.
    For example, Current_value - Current_prev > 0.01 shall be published where "Current_value" is the new value and "Current_prev" is the previously published value.
    
    Signed-off-by: bruce <weichou1229@gmail.com>
    
    (cherry picked from commit f39994077e6603461b5c3fee41b176f810ff9913)
    Signed-off-by: bruce <weichou1229@gmail.com>

--- xrtmodels/schedule.go
+++ xrtmodels/schedule.go
@@ -3,12 +3,13 @@ package xrtmodels
 // Schedule is used to register timed, polled reads of device resources
 // The definition can refer to https://github.com/IOTechSystems/xrt-docs/blob/v2.0-branch/docs/mqtt-management/mqtt-management.md#schedule-format
 type Schedule struct {
-	Name     string      `json:"name"`
-	Device   string      `json:"device"`
-	Resource []string    `json:"resource"`
-	Interval uint64      `json:"interval,omitempty"`
-	OnChange bool        `json:"on_change"`
-	Publish  bool        `json:"publish"`
-	Units    bool        `json:"units"`
-	Options  interface{} `json:"options,omitempty"`
+	Name     string             `json:"name"`
+	Device   string             `json:"device"`
+	Resource []string           `json:"resource"`
+	Interval uint64             `json:"interval,omitempty"`
+	OnChange bool               `json:"on_change"`
+	Bounds   map[string]float64 `json:"bounds"`
+	Publish  bool               `json:"publish"`
+	Units    bool               `json:"units"`
+	Options  interface{}        `json:"options,omitempty"`
 }
